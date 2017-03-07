138636e88b1aa007ab47ca514db0c9c772e4c1ad
Alexandre Krispin <k.m.alexandre@gmail.com>
2017-03-07 17:52:46 +0900

    Add Gridlist with material-ui

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -1,32 +1,53 @@
 import React, { Component } from 'react'
+import { GridList, GridTile } from 'material-ui/GridList'
 
+
+const styles = {
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-arond',
+    },
+    gridList: {
+        width: 1000,
+        height: 950,
+        overflowY: 'auto',
+    },
+};
 
 export default class Gallery extends Component {
     render() {
         return  (
-            <div>
+            <div style={styles.root}>
+                <GridList
+                    cols={5}
+                    cellHeight={180}
+                    style={styles.gridList}
+                    padding={1}
+                    style={styles.gridList}
+                >
+
                 {
                     this.props.items.map((item, index) => {
                         let { title, imageLinks, infoLink } = item.volumeInfo;
                         return (
-                            <a 
+                            <GridTile
                                 key={index}
-                                className="book"
-                                href={infoLink}
-                                target="_blank"
+                                title={title}
+                                subtitle={infoLink}
+                                actionPosition='left'
+                                titlePosition='top'
+                                titleBackground='linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)'
                             >
                                 <img 
                                     src={imageLinks !== undefined ? imageLinks.thumbnail : ''}
                                     alt='book image'
-                                    className="book-img"
                                 />
-                                <div className="book-text">
-                                { title }
-                                </div>
-                            </a>
+                            </GridTile>
                         );
                     })
                 }
+                </GridList>
             </div>
         )
     }
--- app/index.js
+++ app/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Global from './components/Global'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 
 ReactDOM.render(
-    <Global />,
+    <MuiThemeProvider>
+        <Global />
+    </MuiThemeProvider>,
     document.getElementById('root')
 )
